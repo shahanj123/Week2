e190068e6dddb782f14458d4b9dabef454e6bb80
Shahan Jayasinghe <ShahanJayasinghe@Shahans-MBP.fios-router.home>
2021-01-25 12:41:27 -0500

    first commit

---
+++ src/Practice.java
@@ -0,0 +1,11 @@
+class Practice {
+        //creating the constructor
+        Practice( ){
+        System.out.println( "hello learner"); 
+        }
+         
+        public static void main(String args[]){
+        Practice ob1 = new Practice( );
+        }
+}
+
